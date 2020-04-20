36dc53b2fd896c11efe178041363fff70a86060e
Namesh Kushantha <44038404+techkush@users.noreply.github.com>
2020-04-20 07:11:20 +0530

    Hello Express

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 'use strict';
+const express = require('express');
+const app = express();
+app.set('port', process.env.PORT || 3000);
 
-console.log("Welcome to ChatCat");
+app.get('/', (req, res, next) => {
+    res.send('<h1>Hello Express</h1>');
+});
+
+app.get('/dashboard', (req, res, next) => {
+    res.send('<h1>Dashboard</h1>');
+});
+
+app.listen(app.get('port'), () => {
+    console.log('ChatCat Running on Port: ', 3000);
+})
